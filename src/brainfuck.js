effd118714c0dd7c1914f8fa1c10ce3e2ff5d47e
Soham Malakar <soham.pc4756@gmail.com>
2022-03-10 10:32:19 +0530

    Add search engine optimization

--- src/brainfuck.js
+++ src/brainfuck.js
@@ -4,8 +4,7 @@ function do_interpret()
     const code = document.getElementById("input").value;
     const output = document.getElementById('output');
     const userInput = document.getElementById('userInput').value;
-    const value = interpret(code, userInput).replace(/\r/g, '');
-    output.value = value;
+    output.value = interpret(code, userInput).replace(/\r/g, '');
 }
 
 function modulo(x, y)
